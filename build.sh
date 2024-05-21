ca790ac7e0138779ddf3d39fd561323631c84772
森森 <kwsensen@163.com>
2024-05-21 13:41:04 +0800

    尝试修改项目名称

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ git clone https://github.com/sensen1/circuitjs1.git
 cp ./pom.xml ./circuitjs1/
 
 docker compose run builder
-docker compose build web
+COMPOSE_PROJECT_NAME=bhu_circuitjs1_v1 docker compose build web
