e4abe864c63c1b1cb0bfbad4dd6bd8d99d94d1d8
森森 <kwsensen@163.com>
2024-05-21 20:19:07 +0800

    版本2吧

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ git clone --branch bhu https://github.com/sensen1/circuitjs1.git
 cp ./pom.xml ./circuitjs1/
 
 docker compose run builder
-COMPOSE_PROJECT_NAME=bhu_circuitjs1_v1 docker compose build web
+COMPOSE_PROJECT_NAME=bhu_circuitjs1_v2 docker compose build web
